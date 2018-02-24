3d2b8468c301bd6bb56bc3755f95c0b119246ed7
Oleg <olegwelt@deck-11.local>
2018-02-24 16:52:18 +0200

    OS: Add avatar loading logic

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/RibotsAdapter.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/RibotsAdapter.java
@@ -1,11 +1,18 @@
 package uk.co.ribot.androidboilerplate.ui.main;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +27,11 @@ import uk.co.ribot.androidboilerplate.data.model.Ribot;
 public class RibotsAdapter extends RecyclerView.Adapter<RibotsAdapter.RibotViewHolder> {
 
     private List<Ribot> mRibots;
+    private Context context;
 
     @Inject
-    public RibotsAdapter() {
+    public RibotsAdapter(final Context context) {
+        this.context = context;
         mRibots = new ArrayList<>();
     }
 
@@ -40,10 +49,13 @@ public class RibotsAdapter extends RecyclerView.Adapter<RibotsAdapter.RibotViewH
     @Override
     public void onBindViewHolder(final RibotViewHolder holder, int position) {
         Ribot ribot = mRibots.get(position);
+
         holder.hexColorView.setBackgroundColor(Color.parseColor(ribot.profile().hexColor()));
         holder.nameTextView.setText(String.format("%s %s",
                 ribot.profile().name().first(), ribot.profile().name().last()));
         holder.emailTextView.setText(ribot.profile().email());
+
+        holder.loadAvatar(ribot.profile().avatar());
     }
 
     @Override
@@ -54,12 +66,31 @@ public class RibotsAdapter extends RecyclerView.Adapter<RibotsAdapter.RibotViewH
     class RibotViewHolder extends RecyclerView.ViewHolder {
 
         @BindView(R.id.view_hex_color) View hexColorView;
+        @BindView(R.id.image_avatar) ImageView avatarView;
         @BindView(R.id.text_name) TextView nameTextView;
         @BindView(R.id.text_email) TextView emailTextView;
 
         public RibotViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+        }
+
+        public void loadAvatar(String url) {
+            if (!TextUtils.isEmpty(url)) {
+                Picasso.with(context)
+                        .load(url)
+                        .into(avatarView, new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                hexColorView.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onError() {
+                                Log.e("Oleg", "failed to load an avatar image");
+                            }
+                        });
+            }
         }
     }
 }
